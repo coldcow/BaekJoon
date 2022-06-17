a7c0743fb6cd9d6c6a9afc66820ebb59ebc9990a
coldcow <93986255+coldcow@users.noreply.github.com>
2022-06-17 22:28:20 +0900

    20220617 baekjoon9020

---
+++ baekjoon2/baekjoon9020.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int input = 0, N = 0;
+	int i, j, arr[10001] = { 0, };
+
+	for (j = 2; j < 10001 / j; j++)
+	{
+		if (!arr[j])
+		{
+			for (i = j * j; i < 10001; i += j)
+				if (i % j == 0) arr[i] = 1;
+		}
+	}
+
+	scanf("%d", &input);
+	while (input--)
+	{
+		scanf("%d", &N);
+		for (i = N / 2; i > 1; i--)
+		{
+			if (arr[i] == 1) continue;
+			for (j = N / 2; j <= N; j++)
+			{
+				if (arr[j] == 1) continue;
+				if (i + j == N) goto OUT;
+			}
+		}
+	OUT: printf("%d %d\n", i, j);
+	}
+}
