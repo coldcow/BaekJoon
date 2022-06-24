875d51686be6f4f1a4f53215ba0c396853cb615e
coldcow <93986255+coldcow@users.noreply.github.com>
2022-06-24 22:47:27 +0900

    20220624 baekjoon2798

--- baekjoon2/baekjoon2798.c
+++ baekjoon2/baekjoon2798.c
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int* bubble_sort(int size, int arr[]);
+// int* bubble_sort(int size, int arr[]);
 
 int main(void)
 {
@@ -9,43 +9,42 @@ int main(void)
 	int n = 0, m = 0;
 
 	scanf("%d %d", &n, &m);
+
 	for (int i = 0; i < n; i++)
 		scanf("%d", &input[i]);
 
-	bubble_sort(n, input);
+	// bubble_sort(n, input);
 
-	int near = 999999;
-	for (int i = 0; i < n - 2; i++) {
-		for (int j = i + 1; j < n - 1; j++) {
+	int max = 0;
+	int sum;
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
 			for (int k = j + 1; k < n; k++) {
-				printf("%d %d %d\n", i, j, k);
-				if (m < (input[i] + input[j] + input[k]))
-					continue;
-				if ((m - (input[i] + input[j] + input[k])) < near)
-					near = input[i] + input[j] + input[k];
-
+				sum = input[i] + input[j] + input[k];
+				if ((sum > max) && (sum <= m))
+					max = sum;
 			}
 		}
 		
 	}
 
 
-	printf("%d", near);
+	printf("%d", max);
 
 	return 0;
 }
 
-int *bubble_sort(int size, int arr[])
-{
-	int temp = 0;
-	for (int i = size - 1; i > 0; i--) {
-		for (int j = 0; j < i; j++) {
-			if (arr[j] > arr[j + 1]) {
-				temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
-			}
-		}
-	}
-	return arr;
-}
+//int *bubble_sort(int size, int arr[])
+//{
+//	int temp = 0;
+//	for (int i = size - 1; i > 0; i--) {
+//		for (int j = 0; j < i; j++) {
+//			if (arr[j] > arr[j + 1]) {
+//				temp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = temp;
+//			}
+//		}
+//	}
+//	return arr;
+//}
