efeb0db42010bf94f2d58776fd3536003d9ca5ac
coldcow <93986255+coldcow@users.noreply.github.com>
2022-07-03 17:43:00 +0900

    20220703 baekjoon2108

--- baekjoon2/baekjoon2108.c
+++ baekjoon2/baekjoon2108.c
@@ -1,53 +1,85 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 
-int input[500000] = { 0, };
-void quick_sort(int arr, int n, int L, int R);
+int arr[500001];
+int count[8001];
 
-int main(void)
+int compare(const void* a, const void* b)
 {
-	int n = 0;
-	float sum = 0;
+	int num1 = *(int*)a;
+	int num2 = *(int*)b;
 
-	scanf("%d", &n);
-
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &input[i]);
-		sum += input[i];
-	}
-	quick_sort(input, n, input[0], input[n - 1]);
-	printf("%0.f\n", round(sum / n));
-	printf("%d\n", input[n / 2 + 1]);
-
-	printf("%d\n", input[n - 1] - input[0]);
-
-
+	if (num1 < num2)
+		return -1;
+	if (num1 > num2)
+		return 1;
 	return 0;
 }
 
-void quick_sort(int arr[], int n, int L, int R) {
-	int left = L;
-	int right = R;
-	int temp;
-	int pivot = arr[(L + R) / 2];
+int maxFinder(int arr[], int size_arr)
+{
+	int max = arr[0];
+	int i = 0;
+	while (i < size_arr)
+	{
+		if (max < arr[i])
+			max = arr[i];
+		i++;
+	}
+	return max;
+}
 
-	do {
-		while (arr[left] < pivot)
-			left++;
-		while (arr[right] > pivot)
-			right++;
-		if (left <= right) {
-			temp = arr[left];
-			arr[left] = arr[right];
-			arr[right] = temp;
-			left++;
-			right--;
+int main()
+{
+	int i, n;
+	int sum = 0, mode = 0, flag = 0;
+	scanf("%d", &n);
+	i = 0;
+	while (i < n)
+	{
+		scanf("%d", &arr[i]);
+		sum = sum + arr[i];
+		count[arr[i] + 4000]++;
+		i++;
+	}
+	i = 0;
+	while (i < 8001)
+	{
+		if (maxFinder(count, 8001) == count[i])
+		{
+			flag++;
 		}
-	} while (left <= right);
-
-	if (L < right)
-		quick_sort(arr, n, L, right);
-	if (R > left)
-		quick_sort(arr, n, left, R);
+		i++;
+	}
+	i = 0;
+	while (i < 8001)
+	{
+		if (flag == 1)
+		{
+			if (maxFinder(count, 8001) == count[i])
+			{
+				mode = i - 4000;
+				break;
+			}
+		}
+		else
+		{
+			if (maxFinder(count, 8001) == count[i])
+			{
+				if (flag == 0)
+				{
+					mode = i - 4000;
+					break;
+				}
+				else
+					flag = 0;
+			}
+		}
+		i++;
+	}
+	qsort(arr, n, sizeof(int), compare);
+	printf("%.0f\n", sum / (double)n);
+	printf("%d\n", arr[n / 2]);
+	printf("%d\n", mode);
+	printf("%d\n", arr[n - 1] - arr[0]);
 }
