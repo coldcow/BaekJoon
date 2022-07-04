611d2aa55216bf1bea360d4179b2b0a22143431d
coldcow <93986255+coldcow@users.noreply.github.com>
2022-07-04 20:19:57 +0900

    20220704 baekjoon1427

--- baekjoon2/baekjoon1427.c
+++ baekjoon2/baekjoon1427.c
@@ -2,45 +2,27 @@
 #include <stdio.h>
 #include <string.h>
 
-void insert(int arr[], int len, int num);
-
 int main(void)
 {
-	char input;
-	int arr[12] = { 0, };
+	char temp;
+	char arr[12];
 
-	scanf("%s", &input);
-	int len = 12;
-	int int_input = (int)input;
+	scanf("%s", arr);
 
-	for (int i = 0; i < len; i++) {
-		insert(arr, len, int_input % 10);
-		int_input /= 10;
+	for (int i = strlen(arr) - 1; i >= 0; i--) {
+		for (int j = 0; j < i; j++) {
+			if (arr[j] < arr[j + 1]) {
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
 	}
 
-	for (int i = 0; i < len; i++)
-		printf("%c", (char)arr[i]);
-
+	printf("%s", arr);
 
 	return 0;
 }
 
-// num = 5
-// i = 2
-// 55 33 3
 
-void insert(int arr[], int len, int num) {
-	for (int i = 0; i < len; i++) {
-		if (num >= arr[i]) {
-			for (int j = len - 1; j > i; j--) {
-				arr[j] = arr[j - 1];
-			}
-			arr[i] = num;
-		}
 
-		else {
-			arr[i + 1] = arr[i];
-			arr[i] = num;
-		}
-	}
-}
