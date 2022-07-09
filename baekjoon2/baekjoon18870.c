0b9bdc82bfc2c0502670ef56e11a87d619d865dd
coldcow <93986255+coldcow@users.noreply.github.com>
2022-07-09 22:46:03 +0900

    20220709 baekjoon18870

--- baekjoon2/baekjoon18870.c
+++ baekjoon2/baekjoon18870.c
@@ -1,26 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int input[1000000] = { 0, };
-int count[1000000] = { 0, };
-
 int compare(const void* a, const void* b) {
-
+	return *(int*)a - *(int*)b;
 }
 
-int main(void)
-{
-	int n = 0;
+int unique(int* arr, int size) {
+	int i, j = 0;
+	for (i = 1; i < size;i++) {
+		if (arr[j] == arr[i]) continue;
+		arr[++j] = arr[i];
+	}
+	return ++j;
+}
 
-	scanf_s("%d", &n);
+int binarysearch(int* arr, int size, int key) {
+	int left = 0, right = size - 1, mid;
+	while (left <= right) {
+		mid = (left + right) / 2;
+		if (arr[mid] < key) left = mid + 1;
+		else if (arr[mid] > key) right = mid - 1;
+		else return mid;
+	}
+}
 
-	for (int i = 0; i < n; i++)
-		scanf_s("%d", &input[i]);
+int main() {
+	int n;
+	scanf("%d", &n);
 
-	qsort(input, n, sizeof(int), compare);
+	int* arr = (int*)calloc(n, sizeof(int));
+	int* sort = (int*)calloc(n, sizeof(int));
 
-	for (int i = 0; i < n; i++)
-		printf("%d ", count[i]);
+	for (int i = 0; i < n;i++) {
+		scanf("%d", &arr[i]);
+		sort[i] = arr[i];
+	}
+
+	qsort(sort, n, sizeof(int), compare);
+
+	int cnt = unique(sort, n);
+	for (int i = 0;i < n;i++) {
+		int tmp = binarysearch(sort, cnt, arr[i]);
+		printf("%d ", tmp);
+	}
+	free(arr);
+	free(sort);
 
 	return 0;
 }
