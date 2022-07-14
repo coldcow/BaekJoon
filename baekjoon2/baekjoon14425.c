97c675b3f87bceb9b30af1d9307d85e200042784
coldcow <93986255+coldcow@users.noreply.github.com>
2022-07-14 23:27:39 +0900

    20220714 baekjoon14425 c

--- baekjoon2/baekjoon14425.c
+++ baekjoon2/baekjoon14425.c
@@ -2,33 +2,39 @@
 #include <stdio.h>
 #include <string.h>
 
-char *input[10000];
-char* set[10000];
-
 int main(void)
 {
 	int n, m;
+	char* input[10000];
+	char* set[10000];
 
 	scanf("%d", &n);
 	scanf("%d", &m);
 
 	for (int i = 0; i < n; i++) {
 		scanf("%s", &input[i]);
+		printf("%s", input[i]);
 	}
 
-	for (int i = n; i < m + n; i++) {
+	for (int i = 0; i < m; i++) {
 		scanf("%s", &set[i]);
 	}
+	printf("%s\n", *input[0]);
+	/*for (int i = 0; i < n; i++)
+		printf("%s\n", input[i]);
+	for (int i = 0; i < m; i++)
+		printf("%s\n", set[i]);*/
 
 	int count = 0;
 	for (int i = 0; i < m; i++) {
 		for (int j = 0; j < n; j++) {
-			if (set[i] == input[j]) {
+			if (input[j] == set[i]) {
 				count++;
 				break;
 			}
 		}
 	}
+
 
 	printf("%d", count);
 	return 0;
