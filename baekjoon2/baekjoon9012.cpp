570fec5d9b93b2d6d079c12f0f66a8a0a463379d
coldcow <93986255+coldcow@users.noreply.github.com>
2022-07-22 22:14:46 +0900

    20220722 baekjoon9012

--- baekjoon2/baekjoon9012.cpp
+++ baekjoon2/baekjoon9012.cpp
@@ -1,64 +1,38 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
-typedef char element;
-
-typedef struct {
-	element data[51];
-	int size;
-} Stack;
-
-void push(Stack* stack, char input) {
-	stack->data[stack->size++] = input;
-}
-
-char pop(Stack* stack) {
-	return stack->data[--stack->size];
-}
-
-int main(void)
+int main()
 {
-	Stack stack;
-	stack.size = 0;
-	int n;
-	char input;
-	char input_arr[51];
+	int n, i, j, count;
 	scanf("%d", &n);
-
-	for (int i = 0; i < n; i++) {
-		scanf("%s", &input);
-
-		/*if (strlen(&input) % 2 == 1) {
-			printf("NO\n");
-			continue;
-		}*/
-
-		strcpy(input_arr, &input);
-		// (( -> push
-		// () -> pop
-		// ) -> push x
-		// ( -> push
-		 
-		for (int j = 0; j < strlen(&input); j++) {
-			if (stack.data[stack.size - 1] == '(' && input_arr[j] == ')') {
-				pop(&stack);
-				continue;
+	char str[51];
+	i = 0;
+	while (i < n)
+	{
+		scanf("%s", str);
+		j = 0;
+		count = 0;
+		while (j < strlen(str))
+		{
+			if (str[j] == '(')
+			{
+				count++;
 			}
-
-			push(&stack, input_arr[j]);
+			else
+			{
+				count--;
+			}
+			if (count < 0)
+			{
+				printf("NO\n");
+				break;
+			}
+			j++;
 		}
-		if (stack.size != 0) {
+		if (count == 0)
+			printf("YES\n");
+		else if (count > 0)
 			printf("NO\n");
-			stack.size = 0;
-			continue;
-		}
-		
-		printf("YES\n");
-		input = NULL;
+		i++;
 	}
-
-	free(&stack);
-	return 0;
 }
