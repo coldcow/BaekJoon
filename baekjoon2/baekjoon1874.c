6517d2a18e3cf646c1c0fd17f99a539ef80200e2
coldcow <93986255+coldcow@users.noreply.github.com>
2022-07-28 00:35:14 +0900

    20220728 baekjoon1874

--- baekjoon2/baekjoon1874.c
+++ baekjoon2/baekjoon1874.c
@@ -8,15 +8,34 @@ typedef struct {
 	int size;
 } Stack;
 
+void init_stack(Stack* stack) {
+	stack->size = 0;
+}
+
 int main(void)
 {
 	int n;
+	int num = 1;
+	Stack stack;
+	init_stack(&stack);
 
 	scanf("%d", &n);
 
+	int *arr = (int*)malloc(sizeof(int) * n);
+
 	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+		
+		while (stack.data[stack.size - 1] != arr[i]) {
+			if (stack.data[stack.size - 1] > arr[i])
+				push(&stack, num++);
+			else 
+				pop(&stack);
+		}
 
 	}
+
+	
 
 
 
