efa8360456ee0608baa302788f00718687b15104
coldcow <93986255+coldcow@users.noreply.github.com>
2022-07-27 23:21:27 +0900

    20220727 baekjoon1874

---
+++ baekjoon2/baekjoon1874.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef int element;
+
+typedef struct {
+	int* data;
+	int size;
+} Stack;
+
+int main(void)
+{
+	int n;
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+
+	}
+
+
+
+	return 0;
+}
