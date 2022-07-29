823f9772bccdc0d6601b611d86e965a351e2517b
coldcow <cksdnsla159159@naver.com>
2022-07-29 23:24:30 +0900

    20220729 baekjoon1874

--- baekjoon2/baekjoon1874.c
+++ baekjoon2/baekjoon1874.c
@@ -1,43 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#define SIZE 100000
 
-typedef int element;
+char result[SIZE * 2];
+int stack[SIZE];
+int top = -1;
 
-typedef struct {
-	int* data;
-	int size;
-} Stack;
+int main() {
+    int n;
+    scanf("%d", &n);
 
-void init_stack(Stack* stack) {
-	stack->size = 0;
-}
+    //목표 수열 입력
+    int* arr = (int*)malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
 
-int main(void)
-{
-	int n;
-	int num = 1;
-	Stack stack;
-	init_stack(&stack);
+    int num = 1;
+    int idx = 0, result_idx = 0;
+    while (1) {
+        //스택에 있는 수가 목표 수 보다 낮은 경우
+        //push(+)
+        if (top == -1 || stack[top] < arr[idx]) {
+            stack[++top] = num++;
+            result[result_idx++] = '+';
+        }
+        //스택 상단부분의 수가 목표 수와 같은 경우
+        //pop(-)
+        else if (stack[top] == arr[idx]) {
+            top--;
+            result[result_idx++] = '-';
+            idx++;
+        }
+        //스택의 상단부분의 수가 목표 수 보다 높은경우
+        //원하는 수열을 만들 수 없음
+        else {
+            printf("NO\n");
+            return 0;
+        }
+        if (result_idx == n * 2) break;
+    }
 
-	scanf("%d", &n);
+    //결과 출력
+    for (int i = 0; i < result_idx; i++)
+        printf("%c\n", result[i]);
 
-	int *arr = (int*)malloc(sizeof(int) * n);
-
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &arr[i]);
-		
-		while (stack.data[stack.size - 1] != arr[i]) {
-			if (stack.data[stack.size - 1] > arr[i])
-				push(&stack, num++);
-			else 
-				pop(&stack);
-		}
-
-	}
-
-	
-
-
-
-	return 0;
+    return 0;
 }
